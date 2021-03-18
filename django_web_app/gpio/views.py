94d6bc18a1eb84b26513a1cf1558d5f7983eff04
Inka <inkabreuer@web.de>
2021-03-18 09:01:43 +0100

    update data view in Browser

--- django_web_app/gpio/views.py
+++ django_web_app/gpio/views.py
@@ -11,47 +11,9 @@ def gpio(request):
 
 def display_HubData(request):
     data = models.HubData.objects.all()  # Collect all records from table
-    columns = list()
-    for column in data:
-        row = list()
-        row.append(str(column.timestamp))
-        row.append(column.temp_reg)
-        row.append(column.light_reg_l)
-        row.append(column.light_reg_h)
-        row.append(column.status_reg)
-        row.append(column.on_board_temp_reg)
-        row.append(column.on_board_humidity_reg)
-        row.append(column.on_board_sensor_error)
-        row.append(column.bmp280_temp_reg)
-        row.append(column.bmp280_pressure_reg_l)
-        row.append(column.bmp280_pressure_reg_m)
-        row.append(column.bmp280_pressure_reg_h)
-        row.append(column.bmp280_status)
-        row.append(column.human_detect)
-        columns.append(str(row))
-    response_html = '<br>'.join(columns)
-    return HttpResponse(response_html)
+    return render(request, 'data.html', {'data': data, 'name': 'Hub-Data'})
 
 
 def display_HubCache(request):
     data = models.HubCache.objects.all()  # Collect all records from table
-    columns = list()
-    for column in data:
-        row = list()
-        row.append(str(column.timestamp))
-        row.append(column.temp_reg)
-        row.append(column.light_reg_l)
-        row.append(column.light_reg_h)
-        row.append(column.status_reg)
-        row.append(column.on_board_temp_reg)
-        row.append(column.on_board_humidity_reg)
-        row.append(column.on_board_sensor_error)
-        row.append(column.bmp280_temp_reg)
-        row.append(column.bmp280_pressure_reg_l)
-        row.append(column.bmp280_pressure_reg_m)
-        row.append(column.bmp280_pressure_reg_h)
-        row.append(column.bmp280_status)
-        row.append(column.human_detect)
-        columns.append(str(row))
-    response_html = '<br>'.join(columns)
-    return HttpResponse(response_html)
+    return render(request, 'data.html', {'data': data, 'name': 'Cache-Data'})
